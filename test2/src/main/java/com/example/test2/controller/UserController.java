325c86d7927536f9b954740f8ee86460b779d9a9
namgijeong <dreaming1817@gmail.com>
2025-07-11 10:27:35 +0900

    ajax시에 페이지이동 웹브라우저상 url 이름이 pageNumber 1 고정되어 있어 이름 변경 && 페이징 버튼들 중 현재 페이지인 버튼은 흐리게 조정

--- test2/src/main/java/com/example/test2/controller/UserController.java
+++ test2/src/main/java/com/example/test2/controller/UserController.java
@@ -39,12 +39,17 @@ public class UserController {
 
     @PostMapping("/loginCheck")
     //form 이나 url 파라미터로 넘어온 값은 @RequestBody가 아니다.
-    public String loginCheck(@RequestParam String idInput, @RequestParam String pwdInput, HttpSession session, RedirectAttributes redirectAttributes){
+    public String loginCheck(@RequestParam String idInput, @RequestParam String pwdInput, HttpSession session, RedirectAttributes redirectAttributes) {
         log.info("넘어온 값 : "+idInput+ ", "+pwdInput);
         boolean userLoginOk = userService.userLogin(idInput, pwdInput);
         if (userLoginOk){
             session.setAttribute("loginId", idInput);
-            return "redirect:/user/loginOk/page?pageNumber=1";
+
+            //ajax시 url이 pageNumber = 1 로 계속 보여져서 일부러 숨겼다.
+            redirectAttributes.addFlashAttribute("pageNumber", 1);
+            return "redirect:/user/loginOk/page";
+
+            //return "redirect:/user/loginOk/page?pageNumber=1";
         }
         redirectAttributes.addFlashAttribute("loginAgain", true);
         return "redirect:/user/login";
@@ -56,7 +61,7 @@ public class UserController {
         로그인하지않은 사용자(JSESSIONID를 가지고있지않은 사용자)도 이용할 수 있으므로 required = false로 해둔다.
      */
     @GetMapping("/loginOk/page")
-    public String loginOkPage(@SessionAttribute(name = "loginId", required = false) String loginId, @RequestParam int pageNumber, Model model){
+    public String loginOkPage(@SessionAttribute(name = "loginId", required = false) String loginId, @ModelAttribute("pageNumber") int pageNumber, Model model) {
         if(loginId == null){
             log.warn("비정상적인 접근!");
             return "redirect:/user/login";
@@ -73,7 +78,7 @@ public class UserController {
         버튼을 클릭했을시 ajax로 해당 페이지 내용 반환
      */
     @GetMapping("/loginOk/ajax")
-    public ResponseEntity<?> loginOkAjax(@SessionAttribute(name = "loginId", required = false) String loginId, @RequestParam int pageNumber, Model model){
+    public ResponseEntity<?> loginOkAjax(@SessionAttribute(name = "loginId", required = false) String loginId, @RequestParam int pageNumber, Model model) {
         if(loginId == null){
             log.warn("비정상적인 접근!");
             //return "redirect:/user/login";
